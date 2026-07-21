70652f6615908325ff82f84beb5745fb4280dcbf
srikrishnaa7 <srikrishnaat@gmail.com>
2026-07-21 17:19:59 +0530

    Add requirements

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     
     print("[Flask] Starting Book Recommendation server...")
-    app.run(debug=True, port=5000)
+    app.run(debug=False)
